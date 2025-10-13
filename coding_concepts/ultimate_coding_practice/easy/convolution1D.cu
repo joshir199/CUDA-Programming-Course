ba76b48e5da1b35641393f1ab92ce04c21807d15
Raushan Joshi <raushanjoshi199@gmail.com>
2025-10-13 18:32:47 +0100

    fixed convolution kernel error

--- coding_concepts/ultimate_coding_practice/easy/convolution1D.cu
+++ coding_concepts/ultimate_coding_practice/easy/convolution1D.cu
@@ -6,9 +6,9 @@ using namespace std;
 
 
 #define N 999999
-#define F_len 255
-#define B 64
-#define threadsPerBlock 318
+#define F_len 2047  // filter size
+#define threadsPerBlock 256
+#define M (N-F_len+1) //output size
 
 #define CHECK_CUDA(call) do {                    \
     cudaError_t e = (call);                      \
@@ -21,39 +21,37 @@ using namespace std;
 
 __constant__ float filter[F_len];
 
+
 // 1D convolution of array using the filter of length len.
-// Here, convolution is only in forward direction.
+// Here, convolution is only in forward direction without any padding.
 // e.g: C[i] = Sum(x[i+j]*f[j] For j in [0, F_len-1].
 // thus, it will only have right halo.
-__global__ void convolution(float* a, float* c) {
+__global__ void convolution1DNoPadding(float* input, float* output, int SharedTileSize)
+{
 
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    //int halo = F_len-1;
-    //int blockMemorySize = F_len + halo;
-    __shared__ float cache[threadsPerBlock];
-    int cacheIdx = threadIdx.x;
+  extern __shared__ float cache[];
+  int tileStart = blockIdx.x * blockDim.x; // starting index for each block
 
-    // get the global Index to copy data from input to shared memory
-    // So, Each block will calculate B output and requires (F_len + B -1) inputs
-    // Thus, for each block, the starting index should be (B * blockIdx.x)
-    int gIdx = threadIdx.x + B * blockIdx.x;
-    if(gIdx <N) {
-        cache[cacheIdx] = a[gIdx];
+  for(int i = threadIdx.x ; i<SharedTileSize; i += blockDim.x) { // check for shared memory length limit
+
+    int globalIdx = i + tileStart;
+    if(globalIdx < N) {  // check for input length limit
+      cache[i] = input[globalIdx];
+    } else {
+      cache[i] = 0.0f;
     }
-    __syncthreads();
+  }
+  __syncthreads();
 
 
-    // calculate the convolution on memory loaded in shared memory
-    // Convolution calculation
-    if (gIdx < N) {
-        float sum = 0.0f;
-        // check if the idex is not overflowing in each block ( i + currentThread < threadPerBlock)
-        for (int i = 0; i < F_len && threadIdx.x + i < blockDim.x; i++) {
-            sum += cache[threadIdx.x + i] * filter[i];
-        }
-        c[tid] = sum;
+  int outputId = threadIdx.x + tileStart; // get globalId for output array by combining per block result
+  if(outputId < M) {  // check for output length limit
+    float sum = 0.0f;
+    for(int i =0; i<F_len; i++) {
+      sum += filter[i] * cache[i + threadIdx.x];
     }
-
+    output[outputId] = sum;
+  }
 }
 
 
@@ -62,19 +60,20 @@ int main() {
     cudaEvent_t start, stop;
     CHECK_CUDA(cudaEventCreate(&start));
     CHECK_CUDA(cudaEventCreate(&stop));
-    CHECK_CUDA(cudaEventRecord(start, 0));
 
-    float h_a[N], h_c[N], h_kernel[F_len];
+
+
+    float h_a[N], h_c[M], h_kernel[F_len];
     float *d_a, *d_c;
 
     CHECK_CUDA(cudaMalloc(&d_a, N*sizeof(float)));
-    CHECK_CUDA(cudaMalloc(&d_c, N*sizeof(float)));
+    CHECK_CUDA(cudaMalloc(&d_c, M*sizeof(float)));
 
     // fill data in host device
     for(int i=0; i<N ;i++) {
-        h_a[i] = 1.0f; //(rand() % 90) * 0.018f;
+        h_a[i] = (rand() % 90) * 0.018f;
     }
-    
+
     // fill the constant memory array on host
     for(int i=0;i<F_len;i++) {
         if(i<F_len/2) {
@@ -86,19 +85,22 @@ int main() {
         }
     }
 
+    CHECK_CUDA(cudaEventRecord(start, 0));
+
     CHECK_CUDA(cudaMemcpy(d_a, h_a, N*sizeof(float), cudaMemcpyHostToDevice));
     // Transfer constant memory data from host to constant memory
     CHECK_CUDA(cudaMemcpyToSymbol(filter, h_kernel, F_len*sizeof(float)));
 
-    int blocksPerGrid = (N + threadsPerBlock - 1)/threadsPerBlock;
-    //size_t shmem = (threadsPerBlock + F_len - 1) * sizeof(float);
+    int blocksPerGrid = (M + threadsPerBlock - 1)/threadsPerBlock;
+    int SharedTileSize = threadsPerBlock + F_len - 1;
+    size_t shmem = SharedTileSize * sizeof(float);
 
-    convolution<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_c);
+    convolution1DNoPadding<<<blocksPerGrid, threadsPerBlock, shmem>>>(d_a, d_c, SharedTileSize);
 
     CHECK_CUDA(cudaGetLastError());
     CHECK_CUDA(cudaDeviceSynchronize());
 
-    CHECK_CUDA(cudaMemcpy(h_c, d_c, N*sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK_CUDA(cudaMemcpy(h_c, d_c, M*sizeof(float), cudaMemcpyDeviceToHost));
 
     CHECK_CUDA(cudaEventRecord(stop, 0));
     CHECK_CUDA(cudaEventSynchronize(stop));
@@ -106,9 +108,9 @@ int main() {
     float elapsed_time;
     CHECK_CUDA(cudaEventElapsedTime(&elapsed_time, start, stop));
 
-    cout<<"Elapsed time(in ms) : "<< elapsed_time<<endl;
+    cout<<"Elapsed time(in ms) : "<< elapsed_time<<endl;   // 3.54
 
-    for(int i = 0; i< 50 && i<N; i++) {
+    for(int i = 0; i< 50 && i<M; i++) {
         cout<<"Convolution result at i:"<<i<<", is: "<<h_c[i]<<endl;
     }
 
--- coding_concepts/ultimate_coding_practice/medium/convolution1Dfull.cu
+++ coding_concepts/ultimate_coding_practice/medium/convolution1Dfull.cu
@@ -7,8 +7,6 @@ using namespace std;
 
 #define N 999999
 #define F_len 2047  // filter size
-#define B 64 // tile size
-#define SharedCacheSize (B + F_len - 1)
 #define threadsPerBlock 256
 
 #define CHECK_CUDA(call) do {                    \
@@ -20,55 +18,45 @@ using namespace std;
     }                                               \
 } while(0)
 
-__constant__ float filter[F_len];
+__constant__ float kernel[F_len];
 
 // 1D convolution of array using the filter of length len.
 // Here, convolution is  in 1D direction.
 // e.g: C[i] = Sum(x[i+j]*f[j] For j in [-F_len/2, F_len/2]).
 // thus, it will only have both left and right halo.
-__global__ void convolutionFull(float* a, float* c) {
+__global__ void convolutionFull(float* a, float* c, int SharedCacheSize) {
 
-    __shared__ float cache[SharedCacheSize];  // Ensure total memory < 64KB
-    int cacheIdx = threadIdx.x;
+    extern __shared__ float cache[];  // Ensure total memory < 64KB
+
     int halo = F_len/2;
     // globalId = threadId in block + block_start_Idx - left_halo
-    int gIdx = threadIdx.x + blockIdx.x * B - halo;
+    int tileStart = blockIdx.x * blockDim.x - halo;
+
 
     // When the cache size is very large, each thread can cooperate to load the elements
     // [Coalesced], where each threads reads multiple inputs into the shared memory
-    int elementsPerThread = (SharedCacheSize + threadsPerBlock - 1)/threadsPerBlock;
-
-    for(int load_i = 0; load_i<elementsPerThread; load_i++) {
-        int localCacheIdx = cacheIdx + load_i * threadsPerBlock; // for cache (extra term for coalesce)
-
-        if(localCacheIdx < SharedCacheSize) { // per block data loading
-
-            int globalIdx = gIdx + load_i * threadsPerBlock; // for input array (extra term for coalesce)
-
-            if(globalIdx >=0 && globalIdx<N) {
-                cache[localCacheIdx] = a[globalIdx];
-            } else {
-                cache[localCacheIdx] = 0.0f;
-            }
+    for(int i = threadIdx.x ; i< SharedCacheSize; i += blockDim.x) {
+        int globalIdx = i + tileStart;
+        if(globalIdx >=0 && globalIdx < N) {
+            cache[i] = a[globalIdx];
+        } else {
+            cache[i] = 0.0f;  // to pad the block with halo number of zeros on both side
         }
     }
     __syncthreads();  // ensure all shared memory operations are completed
 
-    // calculate convolution for B inputs per block
-    // It will produce only B outputs per block
-    if(threadIdx.x < B) {
-        // output Idx = threadId + block output length
-        int outIdx = threadIdx.x + blockIdx.x * B;
-        // this will stop the threadIdx to overflow
-        if (outIdx<N) {
 
-            float sum = 0.0f;
-            for(int i = 0;i<F_len; i++) { // already padded with zero
-                sum += filter[i] * cache[threadIdx.x + i];
-            }
-            c[outIdx] = sum;
+    // output Idx = threadId + block output length
+    int outIdx = threadIdx.x + blockIdx.x * blockDim.x;
+
+    // this will stop the threadIdx to overflow
+    if(outIdx<N) {
+        float sum = 0.0f;
+        for(int i = 0;i<F_len; i++) { // already padded with zero
+            sum += kernel[i] * cache[threadIdx.x + i];
         }
 
+        c[outIdx] = sum;
     }
 }
 
@@ -106,12 +94,13 @@ int main() {
 
     CHECK_CUDA(cudaMemcpy(d_a, h_a, N*sizeof(float), cudaMemcpyHostToDevice));
     // Transfer constant memory data from host to constant memory
-    CHECK_CUDA(cudaMemcpyToSymbol(filter, h_kernel, F_len*sizeof(float)));
+    CHECK_CUDA(cudaMemcpyToSymbol(kernel, h_kernel, F_len*sizeof(float)));
 
     int blocksPerGrid = (N + threadsPerBlock - 1)/threadsPerBlock;
-    //size_t shmem = (threadsPerBlock + F_len - 1) * sizeof(float);
+    int SharedTileSize = threadsPerBlock + F_len - 1;
+    size_t shmem = (SharedTileSize) * sizeof(float);
 
-    convolutionFull<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_c);
+    convolutionFull<<<blocksPerGrid, threadsPerBlock, shmem>>>(d_a, d_c, SharedTileSize);
 
     CHECK_CUDA(cudaGetLastError());
     CHECK_CUDA(cudaDeviceSynchronize());
@@ -124,7 +113,7 @@ int main() {
     float elapsed_time;
     CHECK_CUDA(cudaEventElapsedTime(&elapsed_time, start, stop));
 
-    cout<<"Elapsed time(in ms) : "<< elapsed_time<<endl;   // 2.44
+    cout<<"Elapsed time(in ms) : "<< elapsed_time<<endl;   // 3.41
 
     for(int i = 0; i< 50 && i<N; i++) {
         cout<<"Convolution result at i:"<<i<<", is: "<<h_c[i]<<endl;
